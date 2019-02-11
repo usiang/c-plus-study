59a2feb2b87ec0dd834e91f64db6e45b7047b5b0
傅双亮 <shuangliang.fu@baozun.com>
2019-02-11 17:37:25 +0800

    unit 01

---
+++ src/unit-01/char-string.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+/**
+ * c++ 中字符数组的处理
+ */
+
+int main(int argc, char **argv) {
+    /**
+     * 在C/C++中，基础数据类型有 int double float char，没有string.字符串不是基础数据类型，
+     * 在C中，字符串是使用char[] 实现的
+     */
+
+    //一般的字符，字符的定义是通过单引号('')
+    char a = 'a';
+    char b('b');
+
+    cout << "a:" << a << endl;
+    cout << "b:" << b << endl;
+
+    // 双引号的字符串是以 '\0' 结尾
+    char s1[5] = "1234";
+    cout << sizeof(s1) << endl;
+    cout << s1 << endl;
+
+    char s2[] = {'a','b','c'};
+    cout << sizeof(s2) << endl;
+    cout << s2 << endl;
+
+    char *s3 = new char[10];
+    cout << s3 << endl;
+    delete[](s3);
+    *s3 = NULL;
+
+    return 0;
+}
