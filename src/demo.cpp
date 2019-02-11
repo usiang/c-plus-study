5dea6f311fc10e7d9b8cc21e6815ccc61850f4e0
傅双亮 <shuangliang.fu@baozun.com>
2019-02-11 13:24:08 +0800

    use cmake

---
+++ src/demo.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    string *a = new string();
+    cin >> *a;
+    cout << *a << endl;
+    return 0;
+}
